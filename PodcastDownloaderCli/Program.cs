3757361476d8a47a7fe3cd590adae5e3b8615d26
Chris <christian.bang@gmail.com>
2018-02-01 14:32:14 +0100

    rework and refactor

--- PodcastDownloaderCli/Program.cs
+++ PodcastDownloaderCli/Program.cs
@@ -9,97 +9,105 @@ namespace PodcastDownloaderCli
 {
     class Program
     {
+        public static string root = GetRootPath();
+
         static void Main(string[] args)
+        {
+            string file = "podcasts.txt";
+            string[] lines = ReadFile(file);
+
+            foreach (string line in lines)
+            {
+                ParseLine(line);
+            }
+        }
+
+        private static string[] ReadFile(string podcastFile)
+        {
+            string[] podcastLines = File.ReadAllLines(podcastFile);
+
+            return podcastLines;
+        }
+
+        private static void ParseLine(string line)
+        {
+            Console.WriteLine("Parsing line: " + line);
+            XmlReader xmlreader = XmlReader.Create(line);
+            SyndicationFeed feed = SyndicationFeed.Load(xmlreader);
+            string podcast = feed.Title.Text;
+            xmlreader.Close();
+           
+            string curPath = Path.Combine(root, podcast);
+            if (!Directory.Exists(curPath))
+            {
+                Console.WriteLine("Creating folder: " + curPath);
+                Directory.CreateDirectory(curPath);
+            }
+
+            SyndicationItem feedItem = feed.Items.ElementAt(0);
+            Uri uri = feedItem.Links[1].Uri;
+            string uriString = feedItem.Links[1].Uri.ToString();
+            var last = uriString.Split('/').Last();
+            string fileName = "";
+
+            if (last.Contains("?"))
+            {
+                fileName = last.Split('?')[0];
+            }
+            else
+            {
+                fileName = last;
+            }
+
+            string filePath = Path.Combine(curPath, fileName);
+
+            if (!File.Exists(curPath))
+            {
+                WebClient client = new WebClient();
+                client.DownloadFile(uri, filePath);
+                Console.WriteLine("New episode found! Downloading to: " + curPath);
+                client.Dispose();
+            }
+            else // if filename exist, check size of file
+            {
+                Console.WriteLine("Checking file sizes with buffer");
+                WebClient client = new WebClient();
+                long length = new System.IO.FileInfo(curPath).Length;
+                Stream myStream = client.OpenRead(uriString);
+                Int64 bytes_total = Convert.ToInt64(client.ResponseHeaders["Content-Length"]);
+                myStream.Close();
+
+                if (bytes_total != length)
+                {
+                    Console.WriteLine("File size difference " + length + " vs " + bytes_total + ". Testing with unique name.");
+                    string uniqueTitle = Path.Combine(podcast, feedItem.PublishDate.ToString("yyyy-MM-dd "), fileName);
+
+                    if (!File.Exists(uniqueTitle))
+                    {
+                        Console.WriteLine("Unique does not exist. Downloading to: " + uniqueTitle);
+                        client.DownloadFile(uri, uniqueTitle);
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No new episode!");
+                }
+                client.Dispose();
+            }
+        }
+
+        private static string GetRootPath()
         {
             string[] config = File.ReadAllLines("config.txt");
             string rootLine = config[0];
             string rootPathSubstr = "root=";
             string rootPath = rootLine.Substring(rootPathSubstr.IndexOf(rootPathSubstr) + rootPathSubstr.Length);
-            Console.WriteLine("Root path config is: " + rootPath);
 
-            string[] podcastLines = File.ReadAllLines("podcasts.txt");
-
-            if (!Directory.Exists(rootPath))
-            {
-                Directory.CreateDirectory(rootPath);                
-            }            
-
-            foreach (string line in podcastLines)
-            {
-                string[] lineParts = line.Split(',');
-                string downloadUrl = lineParts[1];
-                Console.WriteLine("Parsing podcast: " + lineParts[0]);
-                XmlReader reader = XmlReader.Create(downloadUrl);
-                SyndicationFeed feed = SyndicationFeed.Load(reader);
-                string podcastNameFolder = feed.Title.Text;
-                reader.Close();
-
-                string curPath = Path.Combine(rootPath, podcastNameFolder);
-                Console.WriteLine("Path is: " + curPath);
-
-                if (!Directory.Exists(curPath))
-                {                                       
-                    Directory.CreateDirectory(curPath);                    
-                }                
-
-                SyndicationItem item = feed.Items.ElementAt(0);
-                Uri uri = item.Links[1].Uri;
-                string uriString = item.Links[1].Uri.ToString();
-                var last = uriString.Split('/').Last();
-                string fileName = "";
-
-                if (last.Contains("?"))
-                {
-                    fileName = last.Split('?')[0];
-                }
-                else
-                {
-                    fileName = last;
-                }
-
-                string finalPath = Path.Combine(curPath,fileName);
-
-                if (!File.Exists(finalPath))
-                {
-                    WebClient client = new WebClient();
-                    client.DownloadFile(uri, finalPath);
-                    Console.WriteLine("New episode found! Downloading to: " + finalPath);
-                    client.Dispose();
-                }
-                else // if filename exist, check size of file
-                {
-                    Console.WriteLine("Checking file sizes with buffer");
-                    WebClient client = new WebClient();
-                    long length = new System.IO.FileInfo(finalPath).Length;
-                    Stream myStream = client.OpenRead(uriString);                    
-                    Int64 bytes_total = Convert.ToInt64(client.ResponseHeaders["Content-Length"]);
-                    myStream.Close();
-
-                    if (bytes_total != length)
-                    {
-                        Console.WriteLine("File size difference " + length + " vs " + bytes_total + ". Testing with unique name.");
-                        string uniqueTitle = Path.Combine(podcastNameFolder, item.PublishDate.ToString("yyyy-MM-dd "), fileName);
-                        
-                        if (!File.Exists(uniqueTitle))
-                        {
-                            Console.WriteLine("Unique does not exist. Downloading to: " + uniqueTitle);
-                            client.DownloadFile(uri, uniqueTitle);
-                        }
-                        else
-                        {
-
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("No new episode!");
-                    }
-                    client.Dispose();
-                }              
-            }
-
-            Console.WriteLine("Finished downloading latest podcasts! Press any key.");
-            Console.ReadKey();
+            return rootPath;
         }
     }
 }
